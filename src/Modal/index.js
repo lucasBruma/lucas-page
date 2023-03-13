a9f85ed004b25d4ba36bf91033efacd1ecaf698a
lucasBruma <lucasbrumatti99@gmail.com>
2023-03-12 22:55:40 -0300

    modal diseño hecho

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Text, VStack, Heading, Stack, Image, Link, HStack } from '@chakra-ui/react';
+import { Box, Text, VStack, Heading, Stack, Image, Link, HStack, Button } from '@chakra-ui/react';
 import { FaGithub, FaLink, FaLinkedin,  } from 'react-icons/fa';
 import { AiFillMail  } from 'react-icons/ai';
 
@@ -7,20 +7,34 @@ import { AiFillMail  } from 'react-icons/ai';
 const Modal = () => {
 
     return (
-        <VStack py='4rem' h='100vh' w='100vw' spacing='3rem' justifyContent='space-around' bgColor='red.veryDark' position='fixed' zIndex='100'>
-            <VStack>
+        <VStack 
+            pt='5rem' 
+            pb='3rem' 
+            h='100vh' 
+            w='100vw' 
+            spacing='2rem' 
+            justifyContent='space-around' 
+            bgColor='red.veryDark' 
+            position='fixed' 
+            zIndex='100'
+        >
+            <VStack spacing='2rem'>
                 <Heading variant='primary' mt='0' lineHeight='.9' fontSize={['3xl', null, 'auto']}>
                     <Box as='span' color='aquamarine'>L</Box>ucas <Box as='span' color='aquamarine'>B</Box>rumatti
                 </Heading>
+                <HStack>
+                    <Button variant='outline'>EN</Button>
+                    <Button variant='outline'>ES</Button>
+                </HStack>
             </VStack>
-            <VStack>
+            <VStack spacing='1.7rem'>
                 <Link>Inicio</Link>
                 <Link>Proyectos</Link>
                 <Link>Sobre mi</Link>
                 <Link>Skills</Link>
                 <Link>Contacto</Link>
             </VStack>
-            <HStack spacing='1rem'>
+            <HStack spacing='3rem' fontSize='1.6rem'>
                 <FaGithub/>
                 <FaLinkedin/>
                 <AiFillMail/>
